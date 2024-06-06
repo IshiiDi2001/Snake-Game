e1d876216ade8b7a67b1e577dc9667dd1d12c0df
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-06-06 18:05:38 +0530

    Random Food Positions Created

---
+++ src/components/Food.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Coordinate } from "../types/type";
+
+export default function Food({ x, y }: Coordinate): JSX.Element {
+  return <Text style={[{ top: y * 10, left: x * 10 }, styles.food]}>🍎</Text>;
+}
+
+const styles = StyleSheet.create({
+  food: {
+    width: 20,
+    height: 20,
+    borderRadius: 7,
+    position: "absolute",
+  },
+});
